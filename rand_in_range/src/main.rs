4cd1036f167e9eb5da499a50216ca802dd625713
Jeklah <arthurbowers@gmail.com>
2021-11-07 19:55:13 +0000

    Adding random int and float println

--- rand_in_range/src/main.rs
+++ rand_in_range/src/main.rs
@@ -1,6 +1,9 @@
 use rand::distributions::{Distribution, Uniform};
 
 fn main() {
+    println!("Integer: {}", rng.gen_range(0..10));
+    println!("Float: {}", rng.gen_range(0.0..10.0));
+
     let mut rng = rand::thread_rng();
     let die = Uniform::from(1..7);
 
