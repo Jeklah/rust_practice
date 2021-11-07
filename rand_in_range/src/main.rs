8c07cbf6bbe1925ac0605e7b2adca49878d089af
Jeklah <arthurbowers@gmail.com>
2021-11-07 20:00:23 +0000

    Fixing error, missing use statement.

--- rand_in_range/src/main.rs
+++ rand_in_range/src/main.rs
@@ -1,10 +1,11 @@
 use rand::distributions::{Distribution, Uniform};
+use rand::Rng;
 
 fn main() {
+    let mut rng = rand::thread_rng();
     println!("Integer: {}", rng.gen_range(0..10));
     println!("Float: {}", rng.gen_range(0.0..10.0));
 
-    let mut rng = rand::thread_rng();
     let die = Uniform::from(1..7);
 
     loop {
