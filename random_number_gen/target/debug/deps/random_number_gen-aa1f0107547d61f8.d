238195fcfa0624fc5f24f9e74b8309f0a9aa774c
Jeklah <arthurbowers@gmail.com>
2021-11-07 19:29:41 +0000

    Adding random number generator

---
+++ random_number_gen/target/debug/deps/random_number_gen-aa1f0107547d61f8.d
@@ -0,0 +1,5 @@
+/home/jeklah/Projects/rust_practice/random_number_gen/target/debug/deps/random_number_gen-aa1f0107547d61f8: main.rs
+
+/home/jeklah/Projects/rust_practice/random_number_gen/target/debug/deps/random_number_gen-aa1f0107547d61f8.d: main.rs
+
+main.rs:
---
+++ random_number_gen/target/debug/random_number_gen.d
@@ -0,0 +1,1 @@
+/home/jeklah/Projects/rust_practice/random_number_gen/target/debug/random_number_gen: /home/jeklah/Projects/rust_practice/random_number_gen/main.rs
