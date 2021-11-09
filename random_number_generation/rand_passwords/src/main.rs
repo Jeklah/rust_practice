078b9a2ae3dc580780f606e7a0c9352f97d2884e
Jeklah <arthurbowers@gmail.com>
2021-11-09 22:59:23 +0000

    Adding body of source code for random passwords.

--- random_number_generation/rand_passwords/src/main.rs
+++ random_number_generation/rand_passwords/src/main.rs
@@ -1,3 +1,12 @@
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
 fn main() {
-    println!("Hello, world!");
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+
+    println!("{}", rand_string);
 }
