60e1103f062c9bb730cd77a2f4c822e051ca27fb
Jeklah <arthurbowers@gmail.com>
2021-11-12 20:47:23 +0000

    Finished implementation of cli arg parsing.

--- cli/arg_parsing/src/main.rs
+++ cli/arg_parsing/src/main.rs
@@ -15,5 +15,19 @@ fn main() {
             .long("name")
             .takes_value(true)
             .help("Five less thsn your favourite number."))
-        .get_matches();
+        .get_matches()
+
+    let myfile = matches.value_of("file").unwrap_or("input.txt");
+    println!("The file passed is: {}", myfile);
+
+    let num_str = matches.value_of("num");
+    match num_str {
+        None => println!("No idea what your favourite number is."),
+        Some(s) => {
+            match.s.parse::<i32>() {
+                Ok(n) => println!("Your favourite number must be {}.", n+5),
+                Err(_) => println!("That's not a number! {}", s),
+            }
+        }
+    }
 }
