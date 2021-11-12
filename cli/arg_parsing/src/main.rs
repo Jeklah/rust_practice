a0fca05e16c56fdb98a5094b10fdd73c1961908d
Jeklah <arthurbowers@gmail.com>
2021-11-12 20:42:59 +0000

    Started implementation of arg_parsing using clap.

--- cli/arg_parsing/src/main.rs
+++ cli/arg_parsing/src/main.rs
@@ -1,3 +1,19 @@
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("My Test Program")
+        .version("0.1.0")
+        .author("Jeklah")
+        .about("Learning Rust")
+        .arg(Arg::with_name("file")
+            .short("f")
+            .long("file")
+            .takes_value(true)
+            .help("A cool file."))
+        .arg(Arg::with_name("num")
+            .short("n")
+            .long("name")
+            .takes_value(true)
+            .help("Five less thsn your favourite number."))
+        .get_matches();
 }
